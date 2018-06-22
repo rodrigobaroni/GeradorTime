58f1958467770e558295f6efb2a0fc02ad518ca4
Rodrigo Baroni <rodrigobaroni>
2018-06-22 11:04:02 -0300

    Initial Commit

---
+++ GeradorTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeradorTime
+//
+//  Created by Rodrigo Baroni on 22/06/2018.
+//  Copyright © 2018 rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
